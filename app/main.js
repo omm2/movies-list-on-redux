53130da1a5571337a881dca7b51d5d59627c4494
Anna Pavlova <a_pavlova@wargaming.net>
2016-01-02 15:14:23 +0200

    add redux-simple-route

--- app/main.js
+++ app/main.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import  ReactDOM from 'react-dom';
+
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route } from 'react-router'
+import { createHistory } from 'history'
+import { syncReduxAndRouter, routeReducer } from 'redux-simple-router'
+import reducers from './reducers'
 
 import './sass/main.scss';
 
 import Hello from './components/component.jsx';
 
-main();
+const reducer = combineReducers(Object.assign({}, reducers, {
+  routing: routeReducer
+}))
+const store = createStore(reducer)
+const history = createHistory()
 
-function main() {
-    React.render(<Hello />, document.getElementById('app'));
-}
+syncReduxAndRouter(history, store)
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Hello}>
+        <Route path="foo" component={Hello}/>
+        <Route path="bar" component={Hello}/>
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+)
